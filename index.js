d479604850ad82cab53a2fd341644cb95bcf9683
Rafael Delanhese <rafael.delanhese@yahoo.com.br>
2021-04-09 16:15:04 -0300

    Primeiro commit index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+const tabelas = require('./infraestrutura/tabelas')
+
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro)
+    } else {
+        tabelas.init(conexao)
+                
+        const app = customExpress()
+
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+        
+    }
+})
+
