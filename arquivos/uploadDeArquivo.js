a3359800c5aa53352ec41347574fc680b8d37893
Rafael Delanhese <rafael.delanhese@yahoo.com.br>
2021-04-15 16:46:27 -0300

    Criação de pasta para salvar imagens

---
+++ arquivos/uploadDeArquivo.js
@@ -0,0 +1,6 @@
+const fs = require('fs')
+
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
+    fs.createReadStream(caminho).pipe(fs.createWriteStream(novoCaminho)).on('finish', () => callbackImagemCriada(novoCaminho))        
+}
