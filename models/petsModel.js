9ef7bbaa284472f43a2d5fa54bace03df372ae45
Rafael Delanhese <rafael.delanhese@yahoo.com.br>
2021-04-15 16:45:22 -0300

    Correção na var. de callback uploadDeArquivo

--- models/petsModel.js
+++ models/petsModel.js
@@ -1,16 +1,20 @@
 const conexao = require('../infraestrutura/conexao')
+const uploadDeArquivo = require('../arquivos/uploadDeArquivo')
 
-class PetsModel{
-    adiciona(pet, res){
+class PetsModel {
+    adiciona(pet, res) {
         const query = 'INSERT INTO Pets SET ?'
 
-        conexao.query(query, pet, erro => {
-            if(erro) {
-                console.log(erro)
-                res.status(400).json(erro)
-            } else {
-                res.status(200).json(pet)
-            }
+        uploadDeArquivo(pet.imagem, pet.nome, novoCaminho => {
+            const novoPet = { nome: pet.nome, imagem: novoCaminho }
+            conexao.query(query, novoPet, erro => {
+                if (erro) {
+                    console.log(erro)
+                    res.status(400).json(erro)
+                } else {
+                    res.status(200).json(novoPet)
+                }
+            })
         })
     }
 }
